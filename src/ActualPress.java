2100f41a2433febf1c2ddb65d955d761c6b489b6
PrpgramTraveler <2115655391@qq.com>
2021-03-20 19:48:23 +0800

    2021-03-20

---
+++ src/ActualPress.java
@@ -0,0 +1,8 @@
+/*
+    date:2021-03-20
+    author:王久铭
+    purpose:做为压力实例化界面的实际用途
+ */
+public class ActualPress {
+
+}
---
+++ src/ScatteredAzimuth.java
@@ -0,0 +1,7 @@
+/*
+    date:2021-03-20
+    author:王久铭
+    purpose:做为方位角离散化界面的实际运用
+ */
+public class ScatteredAzimuth {
+}
